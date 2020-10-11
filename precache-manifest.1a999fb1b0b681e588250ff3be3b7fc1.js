f9d3f18c6765b867aa6404f069b8a158d5dba239
unknown <malhotra.jatin1.jm@gmail.com>
2020-10-11 18:15:09 +0530

    Updates

---
+++ precache-manifest.1a999fb1b0b681e588250ff3be3b7fc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5e22f7d67bd5d5186679c3e56b47e7b",
+    "url": "/TENsocialMedia/index.html"
+  },
+  {
+    "revision": "d011e587819b87bed63e",
+    "url": "/TENsocialMedia/static/css/main.1ee5228a.chunk.css"
+  },
+  {
+    "revision": "334edc753a90dec7f567",
+    "url": "/TENsocialMedia/static/js/2.772e0023.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TENsocialMedia/static/js/2.772e0023.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d011e587819b87bed63e",
+    "url": "/TENsocialMedia/static/js/main.2c829f1d.chunk.js"
+  },
+  {
+    "revision": "e7b4299f261926de0abb",
+    "url": "/TENsocialMedia/static/js/runtime-main.fc93d18e.js"
+  }
+]);
